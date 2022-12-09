b13ade07453b826a720cf36fee330720c87e997a
barinskaya <1388zxc@gmail.com>
2022-12-09 12:52:54 +0300

    Добавьте файлы проекта.

---
+++ labwork 15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace labwork_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Удалить часть строки (заключенную в скобки)"; //задние изначальной строки со скобками
+            char[] text_ar = text.ToCharArray(); //создание массива
+            bool flag = true; //задание логической переменной
+            string res = string.Empty; //задание пустой строки
+
+            for (int i = 0; i < text_ar.Length; i++) //цикл, пока верно условие
+            {
+                if (text_ar[i] == '(') flag = false; //задание логического значения, если на некотором индексе строки открывающая скобка
+                else if (text_ar[i] == ')') flag = true; //задание логического значения, если на некотором индексе строки закрывающая скобка
+                else if (flag) res += text_ar[i]; //замена содржимого скобок на пустую строку
+            }
+
+            Console.WriteLine(res); //вывод строки без скобок
+            Console.ReadLine(); //программа ожидает нажатия клавиши для завершения работы
+        }
+    }
+}
